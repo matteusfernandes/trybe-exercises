7e724119cc33c0c64f23c05ad9bf93dc474e8713
Matteus Fernandes <matteus_gbi@hotmail.com>
2021-05-24 14:02:17 -0300

    Exercicio 04 da aula 4.2

--- bloco_4/4.2/script.js
+++ bloco_4/4.2/script.js
@@ -20,3 +20,11 @@ console.log("A soma de todos os elementos do Array é igual a: " + somaTotal);
 let media = somaTotal / numbers.length;
 
 console.log("A média aritimética dos valores é: " + media);
+
+//Exercício 4
+
+if (media > 20){
+  console.log("Valor maior que 20");
+} else {
+  console.log("Valor menor ou igual a 20");
+}
