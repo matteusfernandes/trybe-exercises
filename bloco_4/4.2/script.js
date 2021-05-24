d570eca85774f8997fe4b8ec5cb535265d6c6842
Matteus Fernandes <matteus_gbi@hotmail.com>
2021-05-24 14:01:16 -0300

    Exercicio 02 da aula 4.2

--- bloco_4/4.2/script.js
+++ bloco_4/4.2/script.js
@@ -5,3 +5,12 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 for (let index = 0; index < numbers.length; index += 1){
   console.log(numbers[index]);
 }
+//Exercício 2
+
+let somaTotal = 0;
+
+for (let indexSoma = 0; indexSoma < numbers.length; indexSoma += 1){
+  somaTotal += numbers[indexSoma];
+}
+
+console.log("A soma de todos os elementos do Array é igual a: " + somaTotal);
