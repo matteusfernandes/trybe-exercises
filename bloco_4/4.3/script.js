8bf43d7544e993af2e414e71848363a8b05fde98
Matteus Fernandes <matteus_gbi@hotmail.com>
2021-05-24 20:23:11 -0300

    Exercicio 03 da aula 4.3

--- bloco_4/4.3/script.js
+++ bloco_4/4.3/script.js
@@ -13,7 +13,22 @@ for (let index = 0; index < n; index += 1){
 //Exercício 2
 
   let temp = "";
-  for (let indexSecond = n; indexSecond > 0; indexSecond -= 1){
+  for (let index = n; index > 0; index -= 1){
     temp += "*";
     console.log(temp);
   }
+
+  //Exercício 3
+  
+  temp = "";
+  temp1 = "";
+
+  for (let index = n; index > 0; index -= 1){
+    for (let indexSecond = index -1; indexSecond > 0; indexSecond -= 1){
+      temp1 += " ";
+    }
+    temp += "*";
+    console.log(temp1 + temp);
+    temp1 = "";
+  }
+  
