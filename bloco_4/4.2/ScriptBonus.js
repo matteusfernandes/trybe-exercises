8c96a52704f13dfa55a93620ad93897a7794dea5
Matteus Fernandes <matteus_gbi@hotmail.com>
2021-05-24 17:58:52 -0300

    Parte II do Exercicio Bonus da aula 4.2

--- bloco_4/4.2/ScriptBonus.js
+++ bloco_4/4.2/ScriptBonus.js
@@ -15,3 +15,17 @@ for (let index = 1; index < numbers.length; index += 1) {
 }
 
 console.log(numbers);
+
+//Parte 2
+
+for (let index = 1; index < numbers.length; index += 1) {
+  for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+    if (numbers[index] > numbers[secondIndex]) {
+      let position = numbers[index];
+      numbers[index] = numbers[secondIndex];
+      numbers[secondIndex] = position;
+    }
+  }
+}
+
+console.log(numbers);
