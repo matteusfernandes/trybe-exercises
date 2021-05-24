7f09caef86342979c04382a589f2f8ad89e2183c
Matteus Fernandes <matteus_gbi@hotmail.com>
2021-05-24 18:52:49 -0300

    Parte III do Exercicio Bonus da aula 4.2

--- bloco_4/4.2/ScriptBonus.js
+++ bloco_4/4.2/ScriptBonus.js
@@ -29,3 +29,19 @@ for (let index = 1; index < numbers.length; index += 1) {
 }
 
 console.log(numbers);
+
+//Parte 3
+
+numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let multiplicando = [];
+
+for (let index = 0; index < numbers.length; index += 1){
+  if (index + 1 >= numbers.length){
+    multiplicando.push(numbers[index] * 2)
+  
+  } else{
+    multiplicando.push(numbers[index] * numbers[index + 1])
+  }
+}
+
+console.log(multiplicando);
