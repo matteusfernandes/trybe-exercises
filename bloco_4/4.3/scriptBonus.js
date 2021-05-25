a8427f9c2222b16de05e8cfc74dff807a473f2f9
Matteus Fernandes <matteus_gbi@hotmail.com>
2021-05-24 21:14:45 -0300

    Exercicio Bonus 05 da aula 4.3 - Precisa revisar

---
+++ bloco_4/4.3/scriptBonus.js
@@ -0,0 +1,27 @@
+//Exercicio 5
+
+/*let n = 7;
+let temp = "";
+
+for (index = n; index > 0; index -= 1) {
+  if (index === 7){
+    for (let indexSecond = 0; indexSecond < n; indexSecond += 1) {
+      temp += "*";
+    }
+    console.log(temp);
+    temp = "";
+  
+  } else if (n % 2 != 0 || n === 1) {
+    for (let indexThird = index; indexThird > 0; indexThird -= 1) {
+      if (indexThird % 2 === 0) {
+        temp += "*";
+      }
+      temp += " ";
+    }
+  } else {
+    temp = "";
+    console.log (temp);
+  }
+  console.log(temp);
+  temp = "";
+}*/
