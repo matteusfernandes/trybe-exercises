2dcc5cee4e4f8cbe4c39dffe04964d7fa37c08ff
Matteus Fernandes <matteus_gbi@hotmail.com>
2021-05-26 19:40:24 -0300

    Implementa a Sexta Funçao

--- exercises/javascript-dom-eventos-e-web-storage_1/script.js
+++ exercises/javascript-dom-eventos-e-web-storage_1/script.js
@@ -4,6 +4,7 @@ function amendParagraphs() {
     paragraphs[key].innerText = 'Lorem Ipsum';
   }
 }
+
 function changeColorSides() {
   let colorSides = document.getElementsByClassName('main-content');
   for (let key in colorSides){
@@ -30,15 +31,9 @@ function paragraphsUpperCase() {
   }
 }
 
-/*
-        Aqui você vai modificar os elementos já existentes utilizando apenas as funções:
-        - document.getElementById()
-        - document.getElementsByClassName()
-        - document.getElementsByTagName()
- Crie uma função que mude o texto na tag <p> para uma descrição de como você se vê daqui a 2 anos. (Não gaste tempo pensando no texto e sim realizando o exercício)
- Crie uma função que mude a cor do quadrado amarelo para o verde da Trybe (rgb(76,164,109)).
- Crie uma função que mude a cor do quadrado vermelho para branco.
- Crie uma função que corrija o texto da tag <h1>.
- Crie uma função que modifique todo o texto da tag <p> para maiúsculo.
- Crie uma função que exiba o conteúdo de todas as tags <p> no console.
-        */
+function paragraphsToConsole () {
+  let paragraphs = document.getElementsByTagName('p');
+  for (let key in paragraphs) {
+    console.log(paragraphs[key]);
+  }
+}
