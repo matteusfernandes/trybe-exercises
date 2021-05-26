d0d4611b45268e132ff92f6fcd2db6bf1a9b694f
Matteus Fernandes <matteus_gbi@hotmail.com>
2021-05-26 18:29:53 -0300

    Altera cor de fundo do footer

--- bloco_5/5.1/script.js
+++ bloco_5/5.1/script.js
@@ -1,2 +1,3 @@
 document.getElementById('header-container').style.backgroundColor = 'green';
 document.getElementById('container').style.backgroundColor = 'rgb(242 242 242)';
+document.getElementById('footer-container').style.backgroundColor = 'rgb(0 51 17)';
