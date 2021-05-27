dfb72dda2b63c5228e38546f9a12deff5cec3d15
Matteus Fernandes <matteus_gbi@hotmail.com>
2021-05-27 20:19:24 -0300

    Termina Primeira Tarefa

---
+++ bloco_5/5.2/exercises/script.js
@@ -0,0 +1,4 @@
+//Adicione a tag h1 com o texto Exercício 5.2 - JavaScript DOM como filho da tag body ;
+let newTitle = document.createElement('h1');
+newTitle.innerText = ('Exercício 5.2 - JavaScript DOM como filho da tag body');
+document.body.appendChild(newTitle);
