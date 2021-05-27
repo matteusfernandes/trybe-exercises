64fe8c30db0bfa3167b0381020e9347d426f4fcf
Matteus Fernandes <matteus_gbi@hotmail.com>
2021-05-27 20:28:09 -0300

    Refatora o codigo criando funcao para implementar novas divs

--- bloco_5/5.2/exercises/script.js
+++ bloco_5/5.2/exercises/script.js
@@ -4,6 +4,12 @@ newTitle.innerText = ('Exercício 5.2 - JavaScript DOM como filho da tag body');
 document.body.appendChild(newTitle);
 
 //Adicione a tag div com a classe main-content como filho da tag body ;
-let newDiv = document.createElement('div');
-newDiv.className = 'main-content';
-document.body.appendChild(newDiv);
+function newDiv(classe) {
+  let newDiv = document.createElement('div');
+  newDiv.className = classe;
+  return newDiv;
+}
+
+document.body.appendChild(newDiv('main-content'));
+
+// Adicione a tag div com a classe center-content como filho da tag div criada no passo 2;
