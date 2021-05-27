9dc064f6a02b96c207aa79d5d8f7442c93b26505
Matteus Fernandes <matteus_gbi@hotmail.com>
2021-05-27 15:35:46 -0300

    Cria diretorio 5.2 e Adiciona exercicios da Parte I do conteudo

---
+++ bloco_5/5.2/script.js
@@ -0,0 +1,7 @@
+document.getElementById('elementoOndeVoceEsta').style.color = 'Red';
+document.getElementById('primeiroFilhoDoFilho').innerText = 'Primeiro Filho do Filho';
+document.getElementById('pai').firstElementChild;
+document.getElementById('elementoOndeVoceEsta').previousElementSibling;
+document.getElementById('elementoOndeVoceEsta').nextSibling;
+document.getElementById('elementoOndeVoceEsta').nextElementSibling;
+document.getElementById('pai').lastElementChild.previousElementSibling;
