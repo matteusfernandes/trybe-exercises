95dd98878d9c225941bbdc3a8136565af413aee3
Matteus Fernandes <matteus_gbi@hotmail.com>
2021-05-27 20:23:21 -0300

    Termina Segunda Tarefa

--- bloco_5/5.2/exercises/script.js
+++ bloco_5/5.2/exercises/script.js
@@ -2,3 +2,8 @@
 let newTitle = document.createElement('h1');
 newTitle.innerText = ('Exercício 5.2 - JavaScript DOM como filho da tag body');
 document.body.appendChild(newTitle);
+
+//Adicione a tag div com a classe main-content como filho da tag body ;
+let newDiv = document.createElement('div');
+newDiv.className = 'main-content';
+document.body.appendChild(newDiv);
