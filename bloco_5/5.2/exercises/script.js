d321e3013c39c47c5dc9ea9be32292aa1ee8afff
Matteus Fernandes <matteus_gbi@hotmail.com>
2021-05-27 20:54:53 -0300

    Termina a Quinta Tarefa e refatora o codigo

--- bloco_5/5.2/exercises/script.js
+++ bloco_5/5.2/exercises/script.js
@@ -13,12 +13,12 @@ function newDiv(classe) {
 document.body.appendChild(newDiv('main-content'));
 
 // Adicione a tag div com a classe center-content como filho da tag div criada no passo 2;
-let mainContent = document.querySelector('.main-content');
-let centerContent = newDiv('center-content');
-mainContent.appendChild(centerContent);
+document.querySelector('.main-content').appendChild(newDiv('center-content'));
 
 //Adicione a tag p como filho do div criado no passo 3 e coloque algum texto;
-let paragraph = document.querySelector('.center-content');
 let newParagraph = document.createElement('p');
-newParagraph.innerText = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.';
-paragraph.appendChild(newParagraph);
+newParagraph.innerText = 'Lorem Ipsum is simply dummy text.';
+document.querySelector('.center-content').appendChild(newParagraph);
+
+// Adicione a tag div com a classe left-content como filho da tag div criada no passo 2;
+document.querySelector('.main-content').appendChild(newDiv('left-content'));
