f94b0dde5bebebe92eb82f8157a5327cca86e2f6
Matteus Fernandes <matteus_gbi@hotmail.com>
2021-05-27 16:09:11 -0300

    Adiciona exercicios da Parte II do conteudo

--- bloco_5/5.2/script.js
+++ bloco_5/5.2/script.js
@@ -1,3 +1,4 @@
+// Conteúdo Parte I
 document.getElementById('elementoOndeVoceEsta').style.color = 'Red';
 document.getElementById('primeiroFilhoDoFilho').innerText = 'Primeiro Filho do Filho';
 document.getElementById('pai').firstElementChild;
@@ -5,3 +6,21 @@ document.getElementById('elementoOndeVoceEsta').previousElementSibling;
 document.getElementById('elementoOndeVoceEsta').nextSibling;
 document.getElementById('elementoOndeVoceEsta').nextElementSibling;
 document.getElementById('pai').lastElementChild.previousElementSibling;
+
+// Conteúdo Parte II
+let newBrother = document.querySelector('#pai');
+let brother = document.createElement('div');
+brother.innerText = "Irmão mais Novo";
+newBrother.appendChild(brother);
+
+let newChild = document.querySelector('#elementoOndeVoceEsta');
+let child = document.createElement('div');
+child.innerText = "Filho caçula";
+newChild.appendChild(child);
+
+let firstGrandChild = document.querySelector('#primeiroFilhoDoFilho');
+let grandChild = document.createElement('div');
+grandChild.innerText = "Primeiro Netinho";
+firstGrandChild.appendChild(grandChild);
+
+document.querySelector('#primeiroFilhoDoFilho div').parentNode.parentNode.nextElementSibling;
