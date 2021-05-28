9595659ff66f8d6b3658ef8d0926dd15ffba09fe
Matteus Fernandes <matteus_gbi@hotmail.com>
2021-05-27 21:02:43 -0300

    Termina a Setima Tarefa

--- bloco_5/5.2/exercises/script.js
+++ bloco_5/5.2/exercises/script.js
@@ -3,13 +3,14 @@ let newTitle = document.createElement('h1');
 newTitle.innerText = ('Exercício 5.2 - JavaScript DOM como filho da tag body');
 document.body.appendChild(newTitle);
 
-//Adicione a tag div com a classe main-content como filho da tag body ;
+// Função para criar novas Div's recebendo como parâmetro uma sting com o nome da classe
 function newDiv(classe) {
   let newDiv = document.createElement('div');
   newDiv.className = classe;
   return newDiv;
 }
 
+//Adicione a tag div com a classe main-content como filho da tag body ;
 document.body.appendChild(newDiv('main-content'));
 
 // Adicione a tag div com a classe center-content como filho da tag div criada no passo 2;
@@ -25,3 +26,9 @@ document.querySelector('.main-content').appendChild(newDiv('left-content'));
 
 // Adicione a tag div com a classe right-content como filho da tag div criada no passo 2;
 document.querySelector('.main-content').appendChild(newDiv('right-content'));
+
+// Adicione uma imagem com src configurado para o valor https://picsum.photos/200 e classe small-image . Esse elemento deve ser filho do div criado no passo 5;
+let newImg = document.createElement('img');
+newImg.src = 'https://picsum.photos/200';
+newImg.className = 'small-image'
+document.querySelector('.left-content').appendChild(newImg);
