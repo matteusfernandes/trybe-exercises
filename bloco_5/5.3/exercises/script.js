147e98d811f935456adbda498a7de67fb4b2f550
Matteus Fernandes <matteus_gbi@hotmail.com>
2021-05-30 16:24:26 -0300

    Implementa primeira funcao

--- bloco_5/5.3/exercises/script.js
+++ bloco_5/5.3/exercises/script.js
@@ -12,5 +12,24 @@ function createDaysOfTheWeek() {
 };
 
 createDaysOfTheWeek();
-
 // Escreva seu código abaixo.
+function createDaysList() {
+  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+  
+  for (let key in dezDaysList) {
+    let day = dezDaysList[key];
+    let dayList = document.createElement('li');
+    dayList.innerText = day;
+    if (dayList.innerText === '24' || dayList.innerText === '31') {
+      dayList.className = 'day holiday';
+    } else if (dayList.innerText === '4' || dayList.innerText === '11' || dayList.innerText === '18') {
+      dayList.className = 'day friday';
+    } else if (dayList.innerText === '25'){
+      dayList.className = 'day friday holiday';
+    } else {
+      dayList.className = 'day';
+    }
+    document.querySelector('#days').appendChild(dayList); 
+  }
+}
+createDaysList();
