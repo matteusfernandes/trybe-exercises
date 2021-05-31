779e4ff8aa1b27c832e73d370f4a8e465633c12c
Matteus Fernandes <matteus_gbi@hotmail.com>
2021-05-31 02:52:58 -0300

    Exercicio 1

---
+++ bloco_4/4.4/parteI.js
@@ -0,0 +1,8 @@
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+};
+
+// 1
+console.log('Bem-vinda, ' + info.personagem);
