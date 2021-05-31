f7d7d7a7811fdd1328340be306b581264e2736ad
Matteus Fernandes <matteus_gbi@hotmail.com>
2021-05-31 20:53:35 -0300

    Cria container para as cores de fundo

--- bloco_5/5.4/script.js
+++ bloco_5/5.4/script.js
@@ -1,6 +1,7 @@
 // Declara variáveis dos containers do html
 const head = document.querySelector('.head-container');
 const main = document.querySelector('.main-container');
+const colors = document.querySelector('.colors-display');
 
 // Função para implementar novos elementos
 function newElement(type, text, dad) {
@@ -36,3 +37,9 @@ newElement('h2', newTitle, main);
 // Cria um parágrafo;
 paragraphText = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).";
 newElement('p', paragraphText, main);
+
+// Cria título para seletor de cor de fundo
+newTitle = 'Escolha a cor de fundo da página:'
+newElement('h3', newTitle, colors);
+
+// FUNÇÃO PARA MUDAR A COR DE FUNDO DA PÁGINA
