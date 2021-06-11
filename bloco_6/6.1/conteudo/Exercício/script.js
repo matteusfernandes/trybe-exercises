3a4e21fea98265bee6260ad1bb2735867a0099d8
Matteus Fernandes <matteus_gbi@hotmail.com>
2021-06-10 22:06:53 -0300

    Implementa Resultado do formulario

--- bloco_6/6.1/conteudo/Exercício/script.js
+++ bloco_6/6.1/conteudo/Exercício/script.js
@@ -59,21 +59,29 @@ function validMonth(monthToValid) {
 
 // Função para separar dia, mês e ano e chamar verificações
 function setTheDate() {
-  const date = document.querySelector('#start-date');
+  const date = document.querySelector('#start-date').value;
   let dateIsValid = true;
   
-  if (date.value.length === 8) {
-    const dateString = String(date.value);
+  if (date.length === 8) {
+
+    // Monta o Dia
+    const dateString = String(date);
     const day = dateString[0] + dateString[1];
     
     // Verifica se o Dia é válido
     const verifyDay = validDay(day);
+
+    // Monta o Mês e verifica se é válido
     const month = dateString[2] + dateString[3];
     const verifyMonth = validMonth(month);
+
+    // Monta o Ano
     let year = '';
     for (let index = 4; index < dateString.length; index += 1) {
       year += dateString[index];
     }
+
+    // Verificações Gerais e retorno
     if (verifyDay === false) {
       alert('Dia inválido!');
       dateIsValid = false;
@@ -90,7 +98,10 @@ function setTheDate() {
       dateIsValid = false;
       return dateIsValid;
     }
+    return dateIsValid;
   }
+  dateIsValid = false;
+  return dateIsValid;
 }
 
 // Função para checar se o nome foi digitado
@@ -141,7 +152,7 @@ function checkAdress() {
 
 // Função para checar se a cidade é válida
 function checkCity() {
-  const city = document.getElementById('city-input').value;
+  const city = document.getElementById('city').value;
 
   if (city.length > 0 && city.length <=28) {
     return true;
@@ -180,22 +191,138 @@ function checkJobDescription() {
   return false;
 }
 
+// Função pra montar o cpf
+function makeACpf() {
+  const cpfNumber = document.getElementById('cpf-number').value;
+  const newNumber = cpfNumber.split('');
+  newNumber.splice(3, 0, '.');
+  newNumber.splice(7, 0, '.');
+  newNumber.splice(11, 0, '-');
+  return newNumber.join('');
+}
+
+// Função para checar qual complemento foi marcado
+function checkComp() {
+  const apt = document.getElementById('apt-type').checked;
+  const house = document.getElementById('house-type').checked;
+  let result = 'Apartamento';
+  if (house === true) {
+    result = 'Casa';
+    return result;
+  }
+  return result;
+}
+
+// Função para montar a data
+function assembleDate() {
+  const dateNumber = document.getElementById('start-date').value;
+  const newAssembleDate = dateNumber.split('');
+  newAssembleDate.splice(2, 0, '/');
+  newAssembleDate.splice(5, 0, '/');
+  return newAssembleDate.join('');
+}
+
+// Função que Monta o conteúdo interno da Div
+function innerElements() {
+  // Nome, E-mail, CPF, Endereço, Cidade, Estado, Casa, Resumo, Cago Anterior, Descrição do Cargo, Data
+  const finalDiv = document.getElementById('final-form');
+  // Nome
+  const fullName = document.createElement('p');
+  fullName.innerText = ('Nome: ' + document.getElementById('full-name').value);
+  fullName.className = "line-content";
+  finalDiv.appendChild(fullName);
+
+  //E-mail
+  const email = document.createElement('p');
+  email.innerText = ('E-mail: ' + document.getElementById('email').value);
+  email.className = 'line-content';
+  finalDiv.appendChild(email);
+
+  // CPF
+  const cpf = document.createElement('p');
+  cpf.innerText = ("CPF: " + makeACpf());
+  cpf.className = 'line-content';
+  finalDiv.appendChild(cpf);
+
+  // Endereço
+  const adress = document.createElement('p');
+  adress.innerText = ('Endereço: ' + document.getElementById('full-adress').value);
+  adress.className = 'line-content';
+  finalDiv.appendChild(adress);
+
+  // Cidade
+  const city = document.createElement('p');
+  city.innerText = ('Cidade: ' + document.getElementById('city').value);
+  city.className = 'line-content';
+  finalDiv.appendChild(city);
+
+  // Estado
+  const state = document.createElement('p');
+  const nameOfState = document.getElementById('states').value;
+  state.innerText = ('Estado: ' + nameOfState.toUpperCase());
+  state.className = 'line-content';
+  finalDiv.appendChild(state);
+
+  //Complemento
+  const comp = document.createElement('p');
+  comp .innerText = ('Complemento: ' + checkComp());
+  comp.className = 'line-content';
+  finalDiv.appendChild(comp);
+
+  //Resumo do Currículo
+  const summary = document.createElement('div');
+  summary.innerText = ('Resumo do Currículo: ' + document.getElementById('resume').value);
+  summary.className = 'line-content';
+  finalDiv.appendChild(summary);
+
+  // Último cargo ocupado
+  const job = document.createElement('p');
+  job.innerText = ('Último Cargo Ocupado: ' + document.getElementById('office').value);
+  job.className = 'line-content';
+  finalDiv.appendChild(job);
+  
+  // Descrição do último cargo ocupado
+  const jobDescription = document.createElement('p');
+  jobDescription.innerText = ('Descrição do Cargo: ' + document.getElementById('description').value);
+  jobDescription.className = 'line-content';
+  finalDiv.appendChild(jobDescription);
+
+  // Data de início
+  const initialDate = document.createElement('p');
+  initialDate.innerText = ("Data de Início: " + assembleDate());
+  initialDate.className = 'line-content';
+  finalDiv.appendChild(initialDate);
+}
+
+// Função para montar a Div com o resultado do form
+function formResult(result) {
+  const formIsValid = result;
+
+  if (formIsValid === true) {
+    const mainContent = document.getElementById('main-content');
+    const newContent = document.createElement('div');
+    newContent.id = 'final-form';
+    mainContent.appendChild(newContent);
+    innerElements();
+    mainContent.firstElementChild.remove();
+  } else {
+    const mainContent = document.getElementById('main-content');
+    mainContent.firstElementChild.remove();
+    const newContent = document.createElement('div');
+    newContent.innerText = 'O FORMULÁRIO CONTÉM DADOS INVÁLIDOS!';
+    mainContent.appendChild(newContent);
+  }
+}
+
 // Função que interrompe o fluxo automático do form
 const btnSend = document.querySelector('#send-button');
 btnSend.addEventListener('click', function (event) {
   event.preventDefault();
-  validadeData();
+  const validForm = validadeData();
+  formResult(validForm);
 });
 
 // Função que inicia o processo de validação
 function validadeData() {
-  setTheDate();
-  checkName();
-  checkEmail();
-  checkCpf();
-  checkAdress();
-  checkCity();
-  checkSummary();
-  checkJob();
-  checkJobDescription();
+  return setTheDate() && checkName() && checkEmail() && checkCpf() && checkAdress() && checkCity() && checkSummary() && checkJob() && checkJobDescription();
 }
