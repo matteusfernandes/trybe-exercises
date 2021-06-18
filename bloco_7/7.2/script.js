363005b312631b854433d998b6568d55793a582b
Matteus Fernandes <matteus_gbi@hotmail.com>
2021-06-18 20:41:22 -0300

    Update no cabecalho dos exercicios

--- bloco_7/7.2/script.js
+++ bloco_7/7.2/script.js
@@ -18,34 +18,34 @@ const lesson3 = {
   turno: 'noite',
 };
 
-// Crie uma função para adicionar o turno da manhã na lesson2 . Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela.
+// 1. Crie uma função para adicionar o turno da manhã na lesson2 . Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela.
 function newTurn(object, key, value) {
   object[key] = value;
 }
 newTurn(lesson2, 'turno', 'manhã');
 
-// Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro.
+// 2. Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro.
 function allKeys(object) {
   console.log(Object.keys(object));
 }
 
-// Crie uma função para mostrar o tamanho de um objeto.
+// 3. Crie uma função para mostrar o tamanho de um objeto.
 function objectLength(object) {
   const objectToLength = Object.keys(object);
   console.log(objectToLength.length);
 }
 
-// Crie uma função para listar os valores de um objeto. Essa função deve receber um objeto como parâmetro.
+//4. Crie uma função para listar os valores de um objeto. Essa função deve receber um objeto como parâmetro.
 function objectValues(object) {
   for (index in object) {
     console.log(Object.values(object));
   }
 }
 
-// Crie um objeto de nome allLessons , que deve agrupar todas as aulas através do Object.assign . Cada chave desse novo objeto será uma aula, sendo essas chaves: lesson1 , lesson2 e lesson3 . Ao executar o comando console.log(allLessons) 
+// 5. Crie um objeto de nome allLessons , que deve agrupar todas as aulas através do Object.assign . Cada chave desse novo objeto será uma aula, sendo essas chaves: lesson1 , lesson2 e lesson3 . Ao executar o comando console.log(allLessons) 
 const allLessons = Object.assign({}, {lesson1, lesson2, lesson3});
 
-// Usando o objeto criado no exercício 5, crie uma função que retorne o número total de estudantes em todas as aulas
+// 6. Usando o objeto criado no exercício 5, crie uma função que retorne o número total de estudantes em todas as aulas
 function numberOfStudents(object) {
   let total = 0;
   const actualKey = Object.keys(object);
@@ -55,12 +55,13 @@ function numberOfStudents(object) {
   return total;
 };
 
-// Crie uma função que obtenha o valor da chave de acordo com a sua posição no objeto
+// 7. Crie uma função que obtenha o valor da chave de acordo com a sua posição no objeto
 function positionInObject(object, index) {
   const key = Object.entries(object);
   console.log (key[index][1]);
 }
 
+// 8. Crie uma função que verifique se o par (chave / valor) existe na função. Essa função deve possuir três parâmetros, sendo eles: o objeto, o nome da chave e o valor da chave.
 function verifyPair(object, key, value) {
   const verify = Object.entries(object);
   let verificate = false;
