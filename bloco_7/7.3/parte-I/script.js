5ad62805107fa367178a78abd047096623ea815f
Matteus Fernandes <matteus_gbi@hotmail.com>
2021-06-21 19:37:04 -0300

    Exercicio 4 - Funcao myRemoveWithoutCopy

--- bloco_7/7.3/parte-I/script.js
+++ bloco_7/7.3/parte-I/script.js
@@ -72,4 +72,7 @@ assert.deepStrictEqual(myRemoveWithoutCopy(testList, 3), [1, 2, 4]);
 assert.notDeepStrictEqual(myRemoveWithoutCopy(testList, 3), [1, 2, 3, 4]);
 
 // 3. Faça uma chamada para a função myRemoveWithoutCopy e verifique se o array passado por parâmetro sofreu alterações
-assert.deepStrictEqual(myRemoveWithoutCopy(testList, 1), [2, 3, 4]);
+// assert.deepStrictEqual(myRemoveWithoutCopy(testList, 1), [2, 3, 4]);
+
+// 4. Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 5) retorna o array esperado
+assert.deepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 5), [1, 2, 3, 4]);
