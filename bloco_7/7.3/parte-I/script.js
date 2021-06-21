7dd27d1eb39881cbe74ebf2ad7188dfb0c5bd78a
Matteus Fernandes <matteus_gbi@hotmail.com>
2021-06-21 18:34:02 -0300

    Exercicio 3

--- bloco_7/7.3/parte-I/script.js
+++ bloco_7/7.3/parte-I/script.js
@@ -13,5 +13,8 @@ function sum(a, b) {
 // 1. Teste se o retorno de sum(4, 5) é 9
 assert.strictEqual(sum(4,5), 9);
 
-// Teste se o retorno de sum(0, 0) é 0
+// 2. Teste se o retorno de sum(0, 0) é 0
 assert.strictEqual(sum(0, 0), 0);
+
+// 3. Teste se a função sum lança um erro quando os parâmetros são 4 e "5" (string 5)
+assert.throws(sum('4', '5'));
