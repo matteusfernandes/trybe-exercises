fb36aefd7fea08adff856682de9eb4c024eda358
Matteus Fernandes <matteus_gbi@hotmail.com>
2021-06-21 18:56:15 -0300

    Exercicio 2 - Funcao myRemove

--- bloco_7/7.3/parte-I/script.js
+++ bloco_7/7.3/parte-I/script.js
@@ -39,3 +39,6 @@ function myRemove(arr, item) {
 // TESTES UNITÁRIOS
 // 1. Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado
 assert.deepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 4]);
+
+// 2. Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]
+assert.notDeepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 3, 4]);
