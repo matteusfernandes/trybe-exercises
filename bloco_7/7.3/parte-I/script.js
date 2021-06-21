429ac39d6e9d45d06a58b64d6d389d36b1fe8ae4
Matteus Fernandes <matteus_gbi@hotmail.com>
2021-06-21 18:47:10 -0300

    Exercicio 4

--- bloco_7/7.3/parte-I/script.js
+++ bloco_7/7.3/parte-I/script.js
@@ -17,7 +17,7 @@ assert.strictEqual(sum(4,5), 9);
 assert.strictEqual(sum(0, 0), 0);
 
 // 3. Teste se a função sum lança um erro quando os parâmetros são 4 e "5" (string 5)
-// assert.throws(sum(4, '5'));
+assert.throws(sum(4, '5'));
 
 // 4. Teste se a mensagem de erro é "parameters must be numbers" quando realizar a chamada sum(4, "5")
 assert.ifError('parameters must be numbers');
