54a36c150a6cfed63247226ee9e6cd4a6510077a
Matteus Fernandes <matteus_gbi@hotmail.com>
2021-06-21 19:46:07 -0300

    Exercicio 2 - Funcao myFizzBuzz

--- bloco_7/7.3/parte-I/script.js
+++ bloco_7/7.3/parte-I/script.js
@@ -89,3 +89,6 @@ function myFizzBuzz(num) {
 
 // 1. Faça uma chamada com um número divisível por 3 e 5 e verifique se o retorno é o esperado
 assert.strictEqual(myFizzBuzz(15), 'fizzbuzz');
+
+// Faça uma chamada com um número divisível por 3 e verifique se o retorno é o esperado
+assert.strictEqual(myFizzBuzz(3), 'fizz');
