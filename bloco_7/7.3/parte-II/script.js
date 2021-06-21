05c5ed74a38fe3ce1994029d90960f256644bdf3
Matteus Fernandes <matteus_gbi@hotmail.com>
2021-06-21 20:20:37 -0300

    Cria a funcao wordLengths

--- bloco_7/7.3/parte-II/script.js
+++ bloco_7/7.3/parte-II/script.js
@@ -1,6 +1,6 @@
 const assert = require('assert');
-// escreva a função addOne aqui
 
+// FUNÇÃO addOne
 function addOne(array) {
   const newArray = [];
   for (let index = 0; index < array.length; index += 1) {
@@ -17,3 +17,17 @@ const output = addOne(myArray);
 assert.strictEqual(typeof addOne, 'function');
 assert.deepStrictEqual(output, expected);
 assert.deepStrictEqual(myArray, unchanged);
+
+// **************************************************************************************
+
+// FUNÇÃO wordLengths
+function wordLengths() {
+
+}
+
+// const words = ['sun', 'potato', 'roundabout', 'pizza'];
+// const expected = [3, 6, 10, 5];
+
+assert.strictEqual(typeof wordLengths, 'function');
+// const output = wordLengths(words);
+// assert.deepStrictEqual(output, expected);
