63c31a8be3c9b3fe3b49f8c5ddfba535c9ca9c5e
Matteus Fernandes <matteus_gbi@hotmail.com>
2021-06-21 18:55:03 -0300

    Exercicio 1 - Funcao myRemove

--- bloco_7/7.3/parte-I/script.js
+++ bloco_7/7.3/parte-I/script.js
@@ -1,5 +1,6 @@
 const assert = require('assert');
 
+// FUNÇÃO sum
 function sum(a, b) {
   if (typeof a !== 'number' || typeof b !== 'number') {
     throw new Error('parameters must be numbers');
@@ -8,16 +9,33 @@ function sum(a, b) {
   return a + b;
 }
 
-// implemente seus testes aqui
-
+// TESTES UNITÁRIOS
 // 1. Teste se o retorno de sum(4, 5) é 9
-assert.strictEqual(sum(4,5), 9);
+// assert.strictEqual(sum(4,5), 9);
 
 // 2. Teste se o retorno de sum(0, 0) é 0
-assert.strictEqual(sum(0, 0), 0);
+// assert.strictEqual(sum(0, 0), 0);
 
 // 3. Teste se a função sum lança um erro quando os parâmetros são 4 e "5" (string 5)
-assert.throws(sum(4, '5'));
+// assert.throws(sum(4, '5'));
 
 // 4. Teste se a mensagem de erro é "parameters must be numbers" quando realizar a chamada sum(4, "5")
-assert.ifError('parameters must be numbers');
+// assert.ifError('parameters must be numbers');
+
+
+// ****************************************************************************************************************************
+
+// FUNÇÃO myRemove
+function myRemove(arr, item) {
+  let newArr = [];
+  for (let index = 0; index < arr.length; index += 1) {
+    if (item !== arr[index]) {
+      newArr.push(arr[index]);
+    }
+  }
+  return newArr;
+}
+
+// TESTES UNITÁRIOS
+// 1. Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado
+assert.deepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 4]);
