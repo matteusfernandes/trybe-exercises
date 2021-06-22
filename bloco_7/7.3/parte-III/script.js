1d5c24aa4add9d3dbb01b6321403bc6270809286
Matteus Fernandes <matteus_gbi@hotmail.com>
2021-06-21 21:06:52 -0300

    Exercicio 2

--- bloco_7/7.3/parte-III/script.js
+++ bloco_7/7.3/parte-III/script.js
@@ -1,6 +1,6 @@
-// Use a variável parameter como parâmetro da função abaixo, escreva testes para verificar se a mesma está retornando como se vê na variável result e, caso não esteja, altere o código para que ele passe nos testes.
 const assert = require('assert');
 
+// 1. Use a variável parameter como parâmetro da função abaixo, escreva testes para verificar se a mesma está retornando como se vê na variável result e, caso não esteja, altere o código para que ele passe nos testes.
 const greetPeople = (people) => {
   let greeting = [];
 
@@ -10,7 +10,36 @@ const greetPeople = (people) => {
   return greeting;
 };
 
-const parameter = ['Irina', 'Ashleigh', 'Elsa'];
-const result = ['Hello Irina', 'Hello Ashleigh', 'Hello Elsa'];
+// const parameter = ['Irina', 'Ashleigh', 'Elsa'];
+// const result = ['Hello Irina', 'Hello Ashleigh', 'Hello Elsa'];
 
-assert.deepStrictEqual(greetPeople(parameter), result);
+// assert.deepStrictEqual(greetPeople(parameter), result);
+
+// *************************************************************************************************
+
+// 2. Use a variável parameter como parâmetro da função abaixo, escreva testes para verificar se a mesma está retornando como se vê na variável result e, caso não esteja, altere o código para que ele passe nos testes. 
+const removeVowels = (word) => {
+  const characters = word.split('');
+  const results = [];
+  let counter = 0;
+
+  for (let index = 0; index < characters.length; index += 1) {
+    if (
+      characters[index] === 'a' ||
+      characters[index] === 'o' ||
+      characters[index] === 'i' ||
+      characters[index] === 'e' ||
+      characters[index] === 'u'
+    ) {
+      counter += 1;
+      results.push(counter);
+    } else {
+      results.push(characters[index]);
+    }
+  }
+  return results.join('');
+};
+
+const parameter = 'Dayane';
+const result = 'D1y2n3';
+assert.strictEqual(removeVowels('Dayane'), 'D1y2n3');
