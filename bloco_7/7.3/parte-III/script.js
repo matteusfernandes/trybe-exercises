1d03468333a098b5ca42ddb4cff28beeab638220
Matteus Fernandes <matteus_gbi@hotmail.com>
2021-06-21 21:12:27 -0300

    Exercicio 4

--- bloco_7/7.3/parte-III/script.js
+++ bloco_7/7.3/parte-III/script.js
@@ -57,9 +57,28 @@ const greaterThanTenNew = (array) => {
   return results;
 };
 
-const parameter = [4, 10, 32, 9, 21];
-const result = [32, 21];
+// const parameter = [4, 10, 32, 9, 21];
+// const result = [32, 21];
 
-assert.strictEqual(typeof greaterThanTenNew, 'function');
-const output = greaterThanTenNew(parameter);
+// assert.strictEqual(typeof greaterThanTenNew, 'function');
+// const output = greaterThanTenNew(parameter);
+// assert.deepStrictEqual(output, result);
+
+// *************************************************************************************************
+
+// 4. Use a variável parameter como parâmetro da função abaixo, escreva testes para verificar se a mesma está retornando como se vê na variável result e, caso não esteja, altere o código para que ele passe nos testes.
+function secondThirdSmallest(array) {
+  let results = []
+  array.sort(function (x, y) {
+      return x - y;
+  });
+  results = [array[1], array[2]];
+  return results;
+};
+
+const parameter = [4, 10, 32, 9, 21, 90, 5, 11, 8, 6];
+const result = [5, 6];
+
+assert.strictEqual(typeof secondThirdSmallest, 'function');
+const output = secondThirdSmallest(parameter);
 assert.deepStrictEqual(output, result);
