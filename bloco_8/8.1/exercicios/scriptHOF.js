e6e9cbd238a8a1a87d030ad70dbf84aac8e3667b
Matteus Fernandes <matteus_gbi@hotmail.com>
2021-06-23 19:43:15 -0300

    Refatora a funcao newEmployees

--- bloco_8/8.1/exercicios/scriptHOF.js
+++ bloco_8/8.1/exercicios/scriptHOF.js
@@ -1,12 +1,12 @@
 // 1 - Crie uma função que retorne um objeto no formato { nomeCompleto, email } representando uma nova pessoa contratada. Passe sua função como parâmetro da HOF newEmployees para criar cada pessoa contratada em seu respectivo id . A sua função deve receber como parâmetro o nome completo da pessoa funcionária e a partir dele gerar automaticamente um email no formato nome_da_pessoa@trybe.com .
 
-const formatName = (id) => id.replace(' ', '_');
+const formatName = (id) => id.toLowerCase().replace(' ', '_');
 
-const newEmployees = () => {
+const newEmployees = (func) => {
   const employees = {
-    id1: generateReg('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
-    id2: generateReg('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
-    id3: generateReg('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+    id1: func('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+    id2: func('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+    id3: func('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
   }
   return employees;
 };
@@ -19,7 +19,7 @@ const generateReg = (id) => {
   return reg;
 }
 
-console.log(newEmployees());
+console.log(newEmployees(generateReg));
 
 // 2 - Desenvolva uma HOF que retorna o resultado de um sorteio. Esta HOF irá gerar um número aleatório entre 1 e 5 recebendo como parâmetros o número apostado e uma função que checa se o número apostado é igual ao número sorteado. O retorno da sua HOF deve ser uma string (Ex: "Tente novamente" ou "Parabéns você ganhou").
 
