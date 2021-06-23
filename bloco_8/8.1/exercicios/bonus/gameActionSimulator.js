46beab2260a903d1086caf6b040b81ad58ab9e55
Matteus Fernandes <matteus_gbi@hotmail.com>
2021-06-23 16:58:56 -0300

    Terceira funcao da Parte I do exercicio bonus

--- bloco_8/8.1/exercicios/bonus/gameActionSimulator.js
+++ bloco_8/8.1/exercicios/bonus/gameActionSimulator.js
@@ -29,3 +29,19 @@ const dragonDamage = () => Math.floor(Math.random() * (dragon.strength)) + 15;
 // O dano será um número aleatório entre o valor do atributo strength (dano mínimo) e o valor de strength * weaponDmg (dano máximo).
 
 const warriorDamage = () => Math.floor(Math.random() * (warrior.strength * warrior.weaponDmg)) + (warrior.strength);
+
+// 3 - Crie uma função que retorna um objeto com duas chaves e dois valores contendo o dano e a mana gasta pelo mago em um turno.
+// O dano será um número aleatório entre o valor do atributo intelligence (dano mínimo) e o valor de intelligence * 2 (dano máximo).
+// A mana consumida por turno é 15. Além disto a função deve ter uma condicional, caso o mago tenha menos de 15 de mana o valor de dano recebe uma mensagem (Ex: "Não possui mana suficiente") e a mana gasta é 0.
+
+const mageDamage = () => {
+  const damageCaused = {
+    value: Math.floor(Math.random() * (mage.intelligence * 2)) + (mage.intelligence),
+    mana: 15,
+  };
+  if (mage.mana < 15) {
+    damageCaused.value = 'Não possui mana suficiente';
+    damageCaused.mana = 0;
+  }
+  return damageCaused;
+}
