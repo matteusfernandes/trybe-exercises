beecb74eaae590e34fe605c6aa2fce313806e7c1
Matteus Fernandes <matteus_gbi@hotmail.com>
2021-06-23 17:19:05 -0300

    Implementa turno do warrior

--- bloco_8/8.1/exercicios/bonus/gameActionSimulator.js
+++ bloco_8/8.1/exercicios/bonus/gameActionSimulator.js
@@ -49,5 +49,10 @@ const mageDamage = () => {
 // ************************ PARTE II ************************
 
 const gameActions = {
-  // Crie as HOFs neste objeto.
+  // 1 - Crie a primeira HOF que compõe o objeto gameActions . Ela será a função que simula o turno do personagem warrior . Esta HOF receberá como parâmetro a função que calcula o dano deferido pelo personagem warrior e atualizará os healthPoints do monstro dragon . Além disto ela também deve atualizar o valor da chave damage do warrior .
+  warriorTurn : () => {
+    const warriorDmgTurn = warriorDamage();
+    dragon.healthPoints -= warriorDmgTurn;
+    warrior.damage = warriorDmgTurn;
+  },
 };
