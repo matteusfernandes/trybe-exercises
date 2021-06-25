c1c17c9a5d991c71551580ca787d7864dd3d2ade
Matteus Fernandes <matteus_gbi@hotmail.com>
2021-06-25 18:14:56 -0300

    Exercicio 6

--- bloco_8/8.3/exercicio/exercise6.js
+++ bloco_8/8.3/exercicio/exercise6.js
@@ -72,6 +72,8 @@ const expectedResult = [
 
 function oldBooks() {
   // escreva seu código aqui
+  const oldBooks = books.filter((book) => (2021 - book.releaseYear) > 60);
+  return oldBooks.map((book => book.name)); 
 }
 
 assert.deepStrictEqual(oldBooks(), expectedResult);
