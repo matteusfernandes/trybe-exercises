ca73bd46da6f7a538edce6914600a71c8f17a25d
Matteus Fernandes <matteus_gbi@hotmail.com>
2021-06-28 20:18:16 -0300

    Exercicio 6

--- bloco_8/8.4/exercicios/exercise6.js
+++ bloco_8/8.4/exercicios/exercise6.js
@@ -6,6 +6,10 @@ const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
 
 function studentAverage() {
   // escreva seu código aqui
+  return students.map((student, index) => ({
+    name: student,
+    average: ((grades[index].reduce((total, current) => total + current)) / grades[index].length),
+  }));
 }
 
 const expected = [
