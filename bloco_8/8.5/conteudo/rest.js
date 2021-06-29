770c9afdcf6358db50d5cdfed2dcd4d943e9b825
Matteus Fernandes <matteus_gbi@hotmail.com>
2021-06-29 17:18:30 -0300

    rest

--- bloco_8/8.5/conteudo/rest.js
+++ bloco_8/8.5/conteudo/rest.js
@@ -1,4 +1,4 @@
 // Nós já aprendemos sobre higher order functions e vimos como o método reduce é útil para somar os elementos de um array. No exemplo acima, a função sum calcula a soma de todos os argumentos passados a ela - independente do número. Como o parâmetro rest "empacota" todos os argumentos em um array, podemos utilizar o reduce para somar tudo o que estiver dentro deste array. Experimente passar mais números como argumento para a função sum . Você verá que independente do número de argumentos passados, a função irá executar a soma. Sua função é muito mais flexível quando queremos passar múltiplos parâmetros com o rest pois você não precisa especificar quantos argumentos a função irá receber!
 
 const sum = (...args) => args.reduce((accumulator, current) => accumulator + current, 0);
-console.log(sum(4, 7, 8, 9, 60)); // 88
+console.log(sum(4, 7, 8, 9, 60, 59, 45, 100, 150)); // 88
