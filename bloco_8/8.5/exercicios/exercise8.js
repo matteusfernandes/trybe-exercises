a3a4bf0e0f7c7a9bb35322fca548c9aaf366d602
Matteus Fernandes <matteus_gbi@hotmail.com>
2021-06-29 18:37:22 -0300

    Exercicio 8

--- bloco_8/8.5/exercicios/exercise8.js
+++ bloco_8/8.5/exercicios/exercise8.js
@@ -3,6 +3,7 @@
 const assert = require('assert');
 
 // escreva greet abaixo
+const greet = (nome, greetings = 'Hi') => `${greetings} ${nome}`;
 
 assert.strictEqual(greet('John'), 'Hi John');
 assert.strictEqual(greet('John', 'Good morning'), 'Good morning John');
