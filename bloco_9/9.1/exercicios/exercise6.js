0b59293bcc5cb76b78aff77f162095dd4a3a061f
Matteus Fernandes <matteus_gbi@hotmail.com>
2021-07-06 18:55:40 -0300

    Exercicio 6

--- bloco_9/9.1/exercicios/exercise6.js
+++ bloco_9/9.1/exercicios/exercise6.js
@@ -13,6 +13,17 @@ const greet = (temperature) => console.log(`Hi there! Curiosity here. Right now 
 const handleError = (errorReason) => console.log(`Error getting temperature: ${errorReason}`);
 
 // definição da função sendMarsTemperature...
+const sendMarsTemperature = (func) => {
+  setTimeout(() => {
+    const didOperationSucceed = Math.random() >= 0.6;
+    if(didOperationSucceed) {
+      func(getMarsTemperature());
+    } else {
+      handleError('Robot is busy');
+    }
+
+  }, messageDelay())  
+}
 
 
 // imprime "It is currently 47ºF at Mars", por exemplo, ou "Error getting temperature: Robot is busy"
