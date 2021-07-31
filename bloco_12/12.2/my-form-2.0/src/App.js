e0b3ce53827511bb33da1a3770fa12975678ed85
Matteus Fernandes <matteus_gbi@hotmail.com>
2021-07-30 21:09:15 -0300

    Termina implementacao dos dados do ultimo emprego

--- bloco_12/12.2/my-form-2.0/src/App.js
+++ bloco_12/12.2/my-form-2.0/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import PersonalData from './components/PersonalData'
+import LastJob from './components/LastJob'
 
 function App() {
   return (
     <form>
       <PersonalData />
+      <LastJob />
     </form>
   );
 }
