57fb0d93c57a26ad0ff6e5b1d0308cff18f7044c
Matteus Fernandes <matteus_gbi@hotmail.com>
2021-08-06 20:04:33 -0300

    Termina implementacao

--- bloco_13/13.1/my-randomuser-app/src/App.js
+++ bloco_13/13.1/my-randomuser-app/src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: true,
+      muser: [],
+    }
+
+    this.fetchMuser = this.fetchMuser.bind(this);
+    this.renderMuserElement = this.renderMuserElement.bind(this);
+  }
+
+  fetchMuser = () => {
+    this.setState({ loading: true }, () => {
+      fetch('https://api.randomuser.me/')
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({
+          loading: false,
+          muser: data.results[0],
+        })
+      })
+    })
+  }
+
+  renderMuserElement() {
+    return (
+      <div>
+        <img src={this.state.muser.picture.thumbnail} alt={this.state.muser.name.first} />
+        <p>{`Nome: ${this.state.muser.name.first} ${this.state.muser.name.last}`}</p>
+        <p>{`Email: ${this.state.muser.email}`}</p>
+        <p>{`Idade: ${this.state.muser.dob.age}`}</p>
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    this.fetchMuser();
+  }
+
+  shouldComponentUpdate({ muser }) {
+    if (muser !== undefined) {
+      console.log('oi')
+      return muser.dob.age <= 50;
+    }
+    return true;
+  }
+
+  render() {
+    const { loading } = this.state;
+    const loadingElement = <span>Loading...</span>;
+
+    return (
+      <div>
+        <p>{loading ? loadingElement : this.renderMuserElement()}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
