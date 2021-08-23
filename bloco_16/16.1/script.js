e62179a8edd1eeff434fffc725bc526edbf79f38
Matteus Fernandes <matteus_gbi@hotmail.com>
2021-08-23 18:55:14 -0300

    Implementa  o switch e store

--- bloco_16/16.1/script.js
+++ bloco_16/16.1/script.js
@@ -2,3 +2,22 @@ const ESTADO_INICIAL = {
   colors: ['white', 'black', 'red', 'green', 'blue', 'yellow'],
   index: 0,
 };
+
+window.onload = function() {
+  const NEXT_COLOR = 'NEXT_COLOR';
+  const PREVIOUS_COLOR = 'PREVIOUS_COLOR';
+
+  function backgroundColorReducer(state = ESTADO_INICIAL, action) {
+    switch(action.type) {
+      case NEXT_COLOR:
+        return {...state, index: state.index === state.colors.length - 1 ? 0 : state.index + 1,};
+
+      case PREVIOUS_COLOR:
+        return;
+      default:
+        return {index: state.index === 0 ? state.colors.length - 1 : state.index - 1,};
+    }
+  }
+}
+
+const store = Redux.createStore(backgroundColorReducer);
